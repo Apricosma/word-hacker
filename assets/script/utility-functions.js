9ed14516cc1ef37a1c5da7c0b671fa5f26b3a55d
Apricosma <113317992+Apricosma@users.noreply.github.com>
2022-11-30 11:48:29 -0600

    Add timer and score class

---
+++ assets/script/utility-functions.js
@@ -0,0 +1,29 @@
+import { welcome, startButton, gameGui } from "./index.js";
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function selectAll(selector, parent = document) {
+    return parent.querySelectorAll(selector);
+}
+
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function hideWelcome() {
+    welcome.style.display = 'none';
+    startButton.style.display = 'none';
+}
+
+function showWelcome() {
+    welcome.style.display = 'flex';
+    startButton.style.display = 'flex';
+}
+
+function showGame() {
+    gameGui.style.display = 'flex';
+}
+
+export { select, selectAll, onEvent, hideWelcome, showWelcome, showGame };
